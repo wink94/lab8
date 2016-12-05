bbfdfef6a51d43083ca78c7838e3408945e8aad0
wink94 <mwindula16@gmail.com>
2016-12-05 23:13:01 +0530

    dgdfgd

--- ex3/ex3/Form1.Designer.vb
+++ ex3/ex3/Form1.Designer.vb
@@ -25,6 +25,7 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
@@ -53,11 +54,21 @@ Partial Class Form1
         Me.Button2.Text = "name"
         Me.Button2.UseVisualStyleBackColor = True
         '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(103, 224)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "total"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(501, 261)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
@@ -70,4 +81,5 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents ListBox1 As ListBox
     Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
--- ex3/ex3/Form1.vb
+++ ex3/ex3/Form1.vb
@@ -1,81 +1,194 @@
 ﻿Imports System.IO
 Public Class Form1
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim sale As Sales = New Sales
-        sale.getZipcodes_And_name()
+        'Dim sale As Sales = New Sales
+        'sale.getZipcodes_And_name()
+
+        getDetailsByZipcodes()
+
 
 
     End Sub
 
-    Class Sales
 
-        Public zipcodes As List(Of String)
-        Public names As List(Of String)
 
-        Sub getZipcodes_And_name()
+    Public zipcodes(5) As String
+    Public names(5) As String
 
-            Dim tempArr() As String
+    Function getDetailsByZipcodes() As Single
 
-            If File.Exists("myfile.txt") Then
-                Dim sr As StreamReader = File.OpenText("myfile.txt")
+        Dim tempArr() As String
+        Dim countCodes As Integer : countCodes = 0
+        Dim codeTotal As Single
+
+        ListBox1.Items.Clear()
+
+        If File.Exists("myfile.txt") Then
+            Dim sr As StreamReader = File.OpenText("myfile.txt")
+
+            While sr.Peek <> -1
+
+                tempArr = Split(sr.ReadLine(), " ")
+
+                If countCodes = zipcodes.GetUpperBound(0) Then
+
+                    ReDim Preserve zipcodes(zipcodes.Length + 5)
+
+                Else
+
+                    If checkIfExist(zipcodes, tempArr(0)) = False Then
+                        zipcodes(countCodes) = tempArr(0)
+                        countCodes += 1
+                    End If
+
+                End If
+
+
+            End While
+
+            sr.Close()
+
+            For Each item As String In zipcodes
+
+                If item Is Nothing Then
+                    Exit For
+                End If
+
+                sr = File.OpenText("myfile.txt")
+                codeTotal = 0
 
                 While sr.Peek <> -1
                     tempArr = Split(sr.ReadLine(), " ")
-                    If checkIfExist(zipcodes, tempArr(0)) = False Then
-                        zipcodes.Add(tempArr(0))
-                    End If
-                    If checkIfExist(names, tempArr(1)) = False Then
-                        zipcodes.Add(tempArr(1))
+
+                    If item = tempArr(0) Then
+                        codeTotal += tempArr(2)
                     End If
 
                 End While
 
-            End If
+                sr.Close()
+                ListBox1.Items.Add(item + " " + CStr(codeTotal))
 
-        End Sub
-
-        Sub byName()
-            Dim total As Integer
-            Dim tempArr() As String
-            Dim tempstr As String
-            If File.Exists("myfile.txt") Then
-                total = 0
-                For Each item As String In zipcodes
-                    Dim sr As StreamReader = File.OpenText("myfile.txt")
-
-                    While sr.Peek <> -1
-                        tempArr = Split(sr.ReadLine(), " ")
-                        If item = tempArr(0) Then
-                            total += tempArr(2)
-                        End If
-                    End While
-
-
-                Next
-
-                tempstr = tempArr + " " + total
-                ListBox1.Items.Add(tempstr)
-
-            End If
-        End Sub
-
-        Function checkIfExist(temp As List(Of String), value As String) As Boolean
-
-            If temp Is Nothing Then
-                Return False
-            End If
-
-            For Each item As String In temp
-                If item = value Then
-                    Return True
-                End If
             Next
 
+
+            Return codeTotal
+
+        Else
+            MessageBox.Show("file doesnt exist")
+            Return -1
+        End If
+
+    End Function
+
+    Function getDetailsByName() As Single
+
+        Dim tempArr() As String
+        Dim countNames As Integer : countNames = 0
+        Dim nameTotal As Single = 0
+
+        ListBox1.Items.Clear()
+
+        If File.Exists("myfile.txt") Then
+            Dim sr As StreamReader = File.OpenText("myfile.txt")
+
+            While sr.Peek <> -1
+
+                tempArr = Split(sr.ReadLine(), " ")
+
+
+
+                If countNames = names.GetUpperBound(0) Then
+
+                    ReDim Preserve names(names.Length + 5)
+
+                Else
+                    If checkIfExist(names, tempArr(1)) = False Then
+                        names(countNames) = tempArr(1)
+                        countNames += 1
+                    End If
+
+                End If
+
+            End While
+
+            sr.Close()
+
+            For Each item As String In names
+
+                If item Is Nothing Then
+                    Exit For
+                End If
+
+                sr = File.OpenText("myfile.txt")
+                nameTotal = 0
+
+                While sr.Peek <> -1
+                    tempArr = Split(sr.ReadLine(), " ")
+
+                    If item = tempArr(1) Then
+                        nameTotal += tempArr(2)
+                    End If
+
+                End While
+
+                sr.Close()
+                ListBox1.Items.Add(item + " " + CStr(nameTotal))
+
+            Next
+
+            Return nameTotal
+
+        Else
+
+            MessageBox.Show("file doesnt exist")
+            Return -1
+
+        End If
+
+    End Function
+
+    Function checkIfExist(temp() As String, value As String) As Boolean
+
+        If temp Is Nothing Then
             Return False
+        End If
 
-        End Function
+        For Each item As String In temp
+            If item = value Then
+                Return True
+            End If
+        Next
+
+        Return False
+
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        getDetailsByName()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        If File.Exists("myfile.txt") Then
+
+            Dim sr As StreamReader = File.OpenText("myfile.txt")
+            Dim tempArr() As String
+            Dim total As Single
 
 
-    End Class
+            While sr.Peek <> -1
 
+                tempArr = Split(sr.ReadLine(), " ")
+
+                total += tempArr(2)
+
+            End While
+
+            MessageBox.Show("Total " + CStr(total))
+
+        End If
+
+    End Sub
 End Class
