b8c9449d44464790b24c27a2c1a7a3a1a4ebacfb
wink94 <mwindula16@gmail.com>
2016-12-08 00:09:37 +0530

    dsfsd

---
+++ Ex1_b/Ex1_b/Form1.vb
@@ -0,0 +1,72 @@
+﻿
+Imports Microsoft.VisualBasic.FileIO
+
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim title, author, dates As String
+
+        If TextBox1.Text <> "" And TextBox2.Text <> "" And TextBox3.Text <> "" Then
+
+            title = TextBox1.Text
+            author = TextBox2.Text
+            dates = TextBox3.Text
+
+            FileSystem.WriteAllText("myfile.txt", title + " " + author + " " + dates + vbNewLine, True)
+
+
+            TextBox1.Clear()
+            TextBox2.Clear()
+            TextBox3.Clear()
+
+        Else
+            MessageBox.Show("empty input")
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim arrSize As Integer = 10
+        Dim arr(arrSize) As String
+        Dim temp() As String
+        Dim count As Integer = 0
+
+        Dim source As TextFieldParser
+
+        source = FileSystem.OpenTextFieldParser("myfile.txt", vbTab)
+
+        While Not source.EndOfData
+            arr(count) = source.ReadLine()
+            count += 1
+
+            If count = arr.GetUpperBound(0) Then
+                ReDim Preserve arr(arr.Length + 5)
+            End If
+
+        End While
+
+
+
+        Array.Sort(arr)
+
+        'Dim sw As StreamWriter = File.CreateText("newfile.txt")
+
+        For Each item As String In arr
+
+            If String.IsNullOrEmpty(item) <> True Then
+                FileSystem.WriteAllText("newfile.txt", item + vbNewLine, True)
+            End If
+
+        Next
+
+        'sw.Close()
+    End Sub
+
+
+
+
+End Class
---
+++ Ex1_b/Ex1_b/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Ex1_b")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Ex1_b")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ad0f18ad-c3ce-457e-97e5-c32f6dd8b0d3")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
